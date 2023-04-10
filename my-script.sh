6c8c0b25b1b2b227925fce2db6d93bc7c0c76931
prashanthvasam <113015838+PrashanthVasam@users.noreply.github.com>
2023-04-10 12:40:43 +0530

    Update my-script.sh

--- my-script.sh
+++ my-script.sh
@@ -13,10 +13,10 @@
                 branch_name=$(basename $GITHUB_REF)
                 if [ "$branch_name" == "release" ] || [ "$branch_name" == "master" ]
                 then
-                    tagname="$newVersion-$branch_name-$GITHUB_RUN_NUMBER }}"
+                    tagname="$newVersion-$branch_name-$GITHUB_RUN_NUMBER"
                     echo "Creating Tag : $tagname"
                     # Output vars are not grabbed by the get source's tag task since it's another job
-                    cd $GITHUB_WORKSPACE/common-utils/
+                    cd $GITHUB_WORKSPACE/
                     git tag "$tagname"
                     git -c http.extraheader="AUTHORIZATION: bearer $secrets.OAUTH_TOKEN" push origin --tags
                 else
