4470ec7b67d52ddb85c0469452aab38194456d03
prashanthvasam <113015838+PrashanthVasam@users.noreply.github.com>
2023-05-05 14:09:13 +0530

    Update helm.bash

--- helm.bash
+++ helm.bash
@@ -8,11 +8,3 @@ aws configure set region $4
 function azureLogin() {
 az login --service-principal -u $1 -p $2 --tenant $3
 }
-function helmpush{
-az acr login --name TOTEMTEST
-helm pull oci://totemtest.azurecr.io/helm/msa-dev-helm --version 0.1.0
-pwd
-ls
-aws ecr get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin 554487920627.dkr.ecr.us-east-1.amazonaws.com
-helm push /home/runner/work/java-maven/java-maven/msa-dev-helm-0.1.0.tgz oci://554487920627.dkr.ecr.us-east-1.amazonaws.com/
-}
